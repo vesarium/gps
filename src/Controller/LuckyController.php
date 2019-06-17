97cec7537bcb95b23b96f8ff7f0af0e8f109e47b
RedEyeCoder <daniil.murashev@gmail.com>
2019-06-18 02:02:44 +0300

    LETs Get Started

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,29 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+     /**
+      * @Route("/lucky/number")
+      */
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class LuckyController extends Controller
+{
+    public function number()
+    {
+        $number = mt_rand(0, 100);
+
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+        ));
+    }
+}

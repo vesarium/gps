f6aa202551851d7a3ba4d78b4f57b9fdb8e49cba
Daniil <daniil@localhost>
2019-07-04 19:40:40 +0100

    dashboard

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,8 +3,32 @@
 
 namespace App\Controller;
 
+use Psr\Container\ContainerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class AdminController
+class AdminController extends AbstractController
 {
+    /**
+     * @Route ("/admin", name="admin_homepage");
+     */
+
+    public function load()
+    {
+        return $this->render('admin/admin-main', [ ]);
+    }
+
+    /**
+     * @Route ("/admin/data-vis");
+     */
+
+    public function showData()
+    {
+        return $this->render('admin/data-visualisation', [ ]);
+    }
+
 
 }

e90cae713dd5751738b3bfd369afecae31465e30
Daniil <daniil@localhost>
2019-07-08 19:55:10 +0100

    database

---
+++ src/Controller/DevicesController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Devices;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class DevicesController extends AbstractController
+{
+    /**
+     * @Route("/devices", name="devices")
+     */
+    public function index()
+    {
+        return $this->render('devices/index.html.twig', [
+            'controller_name' => 'DevicesController',
+
+        ]);
+    }
+
+    /**
+     * @Route("/devices/add", name="add_device")
+     */
+    public function addDevice(): Response
+    {
+        // you can fetch the EntityManager via $this->getDoctrine()
+        // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $product = new Devices();
+        $product->setDeviceName('Dolly2');
+        $product->setDescription('Ergonomic and stylish!');
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$product->getId());
+    }
+}
---
+++ src/Entity/Devices.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\DevicesRepository")
+ */
+class Devices
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $device_name;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDeviceName(): ?string
+    {
+        return $this->device_name;
+    }
+
+    public function setDeviceName(?string $device_name): self
+    {
+        $this->device_name = $device_name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}

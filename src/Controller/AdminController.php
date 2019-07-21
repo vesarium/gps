6a705e54ba0b3c58eb6d00caeb071875b345ab96
Daniil <daniil@localhost>
2019-07-21 22:17:56 +0100

    visual dashboard

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -22,12 +22,39 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route ("/admin/data-vis");
+     * @Route ("/dashboard/devices");
      */
 
     public function showData()
     {
-        return $this->render('admin/data-visualisation.html.twig', [ ]);
+        return $this->render('admin/devices.html.twig', [ ]);
+    }
+
+    /**
+     * @Route ("/dashboard/maps");
+     */
+
+    public function showMaps()
+    {
+        return $this->render('admin/maps.html.twig', [ ]);
+    }
+
+    /**
+     * @Route ("/dashboard/preferences");
+     */
+
+    public function showPreferences()
+    {
+        return $this->render('admin/preferences.html.twig', [ ]);
+    }
+
+    /**
+     * @Route ("/dashboard/login");
+     */
+
+    public function Logout()
+    {
+        return $this->render('admin/login.html.twig', [ ]);
     }
 
 
